256a4822f8cc8904c1f3606faace6ff1585b7849
Sophie Larsen <roxyknit2000@gmail.com>
2025-10-18 20:03:16 -0600

    started adding endpoints. Need to figure out right way to create handlers.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,7 +10,19 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        .delete("/db", this::clearDB)
+                .post("/user", this::registerUser)
+                .post("/session", this::login)
+                .delete("/session", this::logout)
+                .get("/game", this::listGames)
+                .post("/game", this::createGame)
+                .put("/game", this::joinGame)
 
+
+    }
+
+    private static void clearDB(Context ctx){
+        ctx.result("FIller")
     }
 
     public int run(int desiredPort) {
