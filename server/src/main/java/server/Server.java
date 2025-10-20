5d3947b5b10a4bea54db1431af7b75a4ffa3ebb8
Sophie Larsen <roxyknit2000@gmail.com>
2025-10-20 14:46:57 -0600

    Got endpoints to not have error

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,29 +1,36 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
+import com.google.gson.Gson;
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .delete("/db", this::clearDB)
+//                .post("/user", this::registerUser)
+//                .post("/session", this::login)
+//                .delete("/session", this::logout)
+//                .get("/game", this::listGames)
+//                .post("/game", this::createGame)
+//                .put("/game", this::joinGame)
+//                .exception(ResponseException.class, this::exceptionHandler)
+
+        ;
 
         // Register your endpoints and exception handlers here.
-        .delete("/db", this::clearDB)
-                .post("/user", this::registerUser)
-                .post("/session", this::login)
-                .delete("/session", this::logout)
-                .get("/game", this::listGames)
-                .post("/game", this::createGame)
-                .put("/game", this::joinGame)
 
 
     }
 
-    private static void clearDB(Context ctx){
-        ctx.result("FIller")
+    private void clearDB(Context ctx){
+        ctx.result("Filler");
     }
+
+
 
     public int run(int desiredPort) {
         javalin.start(desiredPort);
