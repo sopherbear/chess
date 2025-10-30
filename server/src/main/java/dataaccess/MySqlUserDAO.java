5b858b64df8035f32b263a47574865e49673ca85
Sophie Larsen <roxyknit2000@gmail.com>
2025-10-29 19:58:15 -0600

    Added code for initializing the databases

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -31,15 +31,35 @@ public class MySqlUserDAO implements UserDAO{
 
     private final String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS  pet (
-              `id` int NOT NULL AUTO_INCREMENT,
-              `name` varchar(256) NOT NULL,
-              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
-              `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`id`),
-              INDEX(type),
-              INDEX(name)
+            CREATE TABLE IF NOT EXISTS  user_data (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(password)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS game_data (
+              `gameID` INT NOT NULL,
+              `whiteUsername` varchar(256) NULL,
+              `blackUsername` varchar(256) NULL,
+              `gameName` varchar(256) NOT NULL,
+              `game`  TEXT NOT NULL,
+              PRIMARY KEY (`gameID`),
+              INDEX(whiteUsername),
+              INDEX(blackUsername)
+              )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS auth_data (
+            `authToken` varchar(256) NOT NULL,
+            `username` varchar(256) NOT NULL,
+            PRIMARY KEY (`authToken`)
+            INDEX (username)
+            )
             """
     };
 
